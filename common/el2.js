1aaf3eb4aed7f291e321b72e65bc27b69d596676
世外桃源 <ooxfordck@gmail.com>
2016-08-15 15:12:48 +0800

    up

--- common/el2.js
+++ common/el2.js
@@ -4,7 +4,7 @@
 const elasticsearch = require('elasticsearch'),
     util = require("util"),
     events = require("events");
-const indexName = "legco-";
+const indexName = process.env.SEARCHFARM_INDEXPREFIX || "legco-";
 //const wordfreqProgram = require('wordfreq');
 function elClient(config) {
     this.options = config;
