889ec14d7b39af9f9eef6471d35ade57bcde0b5a
世外桃源 <ooxfordck@gmail.com>
2016-08-18 17:16:17 +0800

    work for display now

--- public/js/services.js
+++ public/js/services.js
@@ -7,10 +7,25 @@
  * v1.2.0
  * MIT License
  */
-String.prototype.trunc = String.prototype.trunc ||
-    function (n) {
-        return (this.length > n) ? this.substr(0, n - 1) + '&hellip;' : this;
+String.prototype.trunc = String.prototype.trunc || function (n) {
+        var char_chinese = /[\u4E00-\u9FCC\u3400-\u4DB5\uFA0E\uFA0F\uFA11\uFA13\uFA14\uFA1F\uFA21\uFA23\uFA24\uFA27-\uFA29]|[\ud840-\ud868][\udc00-\udfff]|\ud869[\udc00-\uded6\udf00-\udfff]|[\ud86a-\ud86c][\udc00-\udfff]|\ud86d[\udc00-\udf34\udf40-\udfff]|\ud86e[\udc00-\udc1d]/;
+
+        var isChinese = char_chinese.test(this);
+        var delim = isChinese ? '…' : '&hellip;';
+        return (this.length > n) ? this.substr(0, n - 1) + delim : this;
     };
+
+function truncateOnWord(str, limit) {
+    var trimmable = '\u0009\u000A\u000B\u000C\u000D\u0020\u00A0\u1680\u180E\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u2028\u2029\u3000\uFEFF';
+    var reg = new RegExp('(?=[' + trimmable + '])');
+    var words = str.split(reg);
+    var count = 0;
+    return words.filter(function (word) {
+        count += word.length;
+        return count <= limit;
+    }).join('');
+}
+
 /* Service to Elasticsearch */
 Calaca.factory('calacaService', ['$q', 'esFactory', '$location', function ($q, elasticsearch, $location) {
         //Set default url if not configured
@@ -39,7 +54,8 @@ Calaca.factory('calacaService', ['$q', 'esFactory', '$location', function ($q, e
                 var i = 0, hitsIn, hitsOut = [], source;
                 hitsIn = (result.hits || {}).hits || [];
                 for (; i < hitsIn.length; i++) {
-                    hitsIn[i]._source.content = hitsIn[i]._source.content.trunc(30);
+                    hitsIn[i]._source.content = hitsIn[i]._source.content.trunc(200);
+                    //hitsIn[i]._source.content = "oenxoienioxnexoi";
                     console.log(hitsIn[i]);
                     source = hitsIn[i]._source;
                     source._id = hitsIn[i]._id;
